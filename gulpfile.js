2f323aa0bcf276f2ed1a1f3c4109668831b1050c
CAtOSe <jasenasp@gmail.com>
2020-08-24 02:34:34 +0300

    Add styles and Gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+function compileSCSS() {
+  return gulp.src('./styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+}
+
+exports.default = () => {
+  gulp.watch('./styles/**/*.*', compileSCSS);
+};
+
+exports.build = compileSCSS;
