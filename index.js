4b0499f76c2598f1737ff031761c36ba2cf45961
CAtOSe <jasenasp@gmail.com>
2020-08-24 02:35:01 +0300

    Add Views and File Browser route

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const browserRouter = require('./routes/browser');
 
+// Create app with settings
 const app = express();
+app.set('view engine', 'ejs');
 
+// Public route at /
+app.use(express.static('public'));
+
+// Homepage
 app.get('/', (req, res) => {
   res.send('Filer works!');
 });
+
+// File browser
+app.use(browserRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
