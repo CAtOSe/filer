4265a02cca3cb48b9f2c2dee330ad7a62f134f5b
CAtOSe <jasenasp@gmail.com>
2020-08-23 23:45:13 +0300

    Add stylelint and eslint

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Filer works!')
+  res.send('Filer works!');
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
+  console.log(`Listening on port ${process.env.PORT}`);
 });
