c5ac2a5e6d290e6996d3e8cfd09b0281650560b1
CAtOSe <jasenasp@gmail.com>
2020-08-23 23:14:57 +0300

    Make Express and dotenv work

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Filer works!')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+});
