ac3a73b816c7c4e097e22727fe957d94dbafbe72
CAtOSe <jasenasp@gmail.com>
2020-08-24 02:53:59 +0300

    Add basic file handling

--- routes/browser.js
+++ routes/browser.js
@@ -1,9 +1,25 @@
+const path = require('path');
 const express = require('express');
+const filesystem = require('../core/filesystem');
 
 const router = express.Router();
 
-router.get('/*', (req, res) => {
-  res.render('browser', { path: req.path });
+router.get('/*', async (req, res) => {
+  const objectPath = path.join(process.env.FILE_PATH, req.path);
+  const object = await filesystem.createObject(objectPath);
+
+  console.log(objectPath);
+  console.log(object);
+
+  if (object !== undefined) {
+    if (object.type === 'dir') {
+      res.render('browser', { path: objectPath });
+    } else {
+      res.send('file');
+    }
+  } else {
+    res.send('404');
+  }
 });
 
 module.exports = router;
