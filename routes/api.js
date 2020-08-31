7608ff9006ab737ffb9c62b3c33becd956c3aeb3
CAtOSe <jasenasp@gmail.com>
2020-08-31 21:05:25 +0300

    Change generic error handling

--- routes/api.js
+++ routes/api.js
@@ -100,7 +100,7 @@ router.use('/', (err, req, res, next) => {
 if (process.env.NODE_ENV === 'production') {
   // eslint-disable-next-line no-unused-vars
   router.use('/', (err, req, res, next) => {
-    res.sendStatus(500);
+    res.status(500).send('An internal server error has occured.');
   });
 }
 
