b7cdd0115fbcb923d9ce069067e6afcb9485943f
CAtOSe <jasenasp@gmail.com>
2020-08-31 21:04:45 +0300

    Add Helmet module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const browserRouter = require('./routes/browser');
 const uploadRouter = require('./routes/upload');
 const apiRouter = require('./routes/api');
@@ -7,6 +8,7 @@ const apiRouter = require('./routes/api');
 // Create app with settings
 const app = express();
 app.set('view engine', 'ejs');
+app.use(helmet());
 
 // Public route at /
 app.use(express.static('public'));
